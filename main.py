88f9f6327806b7bb2755b0f824a00d7821f4bac1
NazarSadovij <145208436+NazarSadovij@users.noreply.github.com>
2023-11-25 17:31:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QTextEdit, QListWidget, QPushButton, QLineEdit,  QVBoxLayout, QHBoxLayout
-
+from PyQt5.QtWidgets import QWidget, QApplication, QTextEdit, QListWidget, QPushButton, QLineEdit,  QVBoxLayout, QHBoxLayout, QInputDialog
+import json
 app  = QApplication([])
 window = QWidget()
 
@@ -13,9 +13,9 @@ tegs_list = QListWidget()
 note_Create_btn = QPushButton("Створити нотатку")
 note_Deleate_btn = QPushButton("Видалити нотатку")
 note_Save_btn = QPushButton("Зберегти нотатку")
-add_Tex_ToNote_btn = QPushButton("додати нотатку до тегу")
-an_Pin_btn = QPushButton("відкріпити нотатку")
-siorch_Note_btn = QPushButton("пошук по тегех")
+add_Tex_ToNote_btn = QPushButton("Додати нотатку до тегу")
+an_Pin_btn = QPushButton("Відкріпити нотатку")
+siorch_Note_btn = QPushButton("Пошук по тегех")
 
 main_line = QHBoxLayout()
 line1 = QVBoxLayout()
@@ -38,16 +38,49 @@ line2.addWidget(siorch_Note_btn)
 
 
 
-
-
-
-
-
-
-
 main_line.addLayout(line1)
 main_line.addLayout(line2)
 window.setLayout(main_line)
+
+def writeFile():
+    with open ("notes.json","w", encoding = "utf-8") as file:
+        json.dump(notes, file, ensure_ascii=True, sort_keys=True, indent=4)
+
+
+
+def save_note():
+    note_text=text.toPlainText()
+    note_name = notes_list.currentItem().text()
+    notes [note_name]["text"] = note_text
+    writeFile()
+
+
+note_Save_btn.clicked.connect(save_note)
+
+notes  = {}
+
+
+def show_note():
+    note_name = notes_list.currentItem().text()
+    text.setText(notes[note_name]["text"])
+
+notes_list.itemClicked.connect(show_note)
+
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Нова замітка", "new_note")
+    if ok and note_name != "":
+        notes [note_name] = {
+            "text": "note_name",
+            "tags":[],
+        }
+        notes_list.addItem(note_name)
+
+with open ("notes.json", "r", encoding="utf-8") as file:
+    notes = json.load(file)
+
+notes_list.addItems(notes)
+
+note_Create_btn.clicked.connect(add_note)
 
 
 
